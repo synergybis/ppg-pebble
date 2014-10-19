0f852879aa27c68f1e4a00e42c74f4a7bbd90364
dstreicher <dstreicher@synergybis.com>
2014-10-19 13:08:50 -0400

    added text layer and click handler methods

---
+++ src/methods.h
@@ -0,0 +1,12 @@
+#pragma once
+
+static void click_handler_select(ClickRecognizerRef recognizer, void *context);
+static void click_handler_up(ClickRecognizerRef recognizer, void *context);
+static void click_handler_down(ClickRecognizerRef recognizer, void *context);
+static void click_config_provider(void *context);
+static void window_load(Window *window);
+static void window_unload(Window *window);
+static void init(void);
+static void deinit(void);
+
+int main(void);
